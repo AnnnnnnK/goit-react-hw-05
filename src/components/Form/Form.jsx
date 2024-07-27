b685ef22cdaa53f467ad9f7b8a9b34a50a9b5d3a
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2024-07-27 10:34:27 +0200

    add styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { form, input, button } from "./Form.module.css";
 
 export const Form = ({ handleSubmit }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -14,14 +15,18 @@ export const Form = ({ handleSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmitForm}>
+    <form onSubmit={handleSubmitForm} className={form}>
       <input
+        className={input}
         type="text"
         name="search"
         value={searchQuery}
         onChange={handleChange}
+        placeholder="Search your favourite film"
       />
-      <button type="submit">Search</button>
+      <button type="submit" className={button}>
+        Search
+      </button>
     </form>
   );
 };
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
-import { list, item, link, text } from "./MovieList.module.css";
+import { list, item, link, text, img } from "./MovieList.module.css";
 
 export const MovieList = ({ movieList }) => {
   const location = useLocation();
   console.log(movieList);
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
   return (
     <ul className={list}>
       {movieList.map(({ backdrop_path, id, original_title }) => (
         <li key={id} className={item}>
           <Link to={`/movies/${id}`} state={location} className={link}>
             <img
-              src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
-              alt=""
+              className={img}
+              src={
+                backdrop_path
+                  ? `https://image.tmdb.org/t/p/w500${backdrop_path}`
+                  : defaultImg
+              }
+              alt="poster"
             />
             <p className={text}>{original_title}</p>
           </Link>
